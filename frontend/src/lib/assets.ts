28a03bb9d0a5d4ff4d4180b0330d17de830c4890
Alexandra Tetiorkina <atrobber@gmail.com>
2026-05-03 21:23:33 +0300

    Add files via upload

---
+++ frontend/src/lib/assets.ts
@@ -0,0 +1,25 @@
+import achiv1 from "../../../../../frontend/new_assets/new_landing/achiv1.png";
+import achiv2 from "../../../../../frontend/new_assets/new_landing/achiv2.png";
+import arrow from "../../../../../frontend/new_assets/new_landing/arrow.png";
+import group from "../../../../../frontend/new_assets/group.png";
+import hands from "../../../../../frontend/new_assets/new_landing/hands.png";
+import mark from "../../../../../frontend/new_assets/orange_icon.png";
+import office from "../../../../../frontend/new_assets/new_landing/office.png";
+import problem from "../../../../../frontend/new_assets/new_landing/problem.png";
+import github from "../../../../../frontend/new_assets/new_social/github_icon.png";
+import google from "../../../../../frontend/new_assets/new_social/google_icon.png";
+
+export const assets = {
+  group,
+  mark,
+  github,
+  google,
+  landing: {
+    problem,
+    hands,
+    achiv1,
+    achiv2,
+    arrow,
+    office,
+  },
+};
